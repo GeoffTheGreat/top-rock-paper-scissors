f85107034c0eeb80a02dd00f52b50a02955b26c2
Timothy <reedtimothy63@gmail.com>
2022-07-14 10:49:41 +0200

    add the algorithm inside the .js file

---
+++ scrip.js
@@ -0,0 +1,16 @@
+//create array of rock paper and scissor rpsArray
+
+//computerPlay function
+//get a random item from the rpsArray
+//return the random item
+
+//player selection
+//create a variable for the userInput
+//get input from the user and assign it to userInput
+//make userInput standard so it can be referenced to the rpsArray
+
+//make a function that takes both the computer play and the user play///playRound
+//use conditional statements to determine who has won
+//return whether or not the user has won
+
+//create a function called game that implements 5 rounds and prints the result on the console
